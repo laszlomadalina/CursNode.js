c978d61ea79a25fa01389389189d27b0caaa91d3
MadalinaL <madalia.laszlo@gmail.com>
2017-07-24 12:17:06 +0300

    ExpressBackend

--- ExpressHelloWorld/index.js
+++ ExpressHelloWorld/index.js
@@ -4,7 +4,15 @@ var app = express();
 app.get('/', function (req, res) {
   res.send('Hello World');
 })
+
+app.post('/', function (req, res) {
+  res.send('No POST method available!');
+})
+
+app.get('/helloworld', function (req, res) {
+  res.send('Hello World !!! 2');
+})
  
 app.listen(3000);
 
-console.log('Server running at http://127.0.0.1: 3000/'); 
+console.log('Server running at http://127.0.0.1:3000/'); 
