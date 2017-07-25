060eed1166e36ab2df9e51c1cfdfb6b4e978b491
MadalinaL <madalia.laszlo@gmail.com>
2017-07-25 10:27:16 +0300

    ProcessExample with beforeExit

--- ProcessExample/index.js
+++ ProcessExample/index.js
@@ -8,4 +8,14 @@ process.on('exit', function(code) {
    console.log('About to exit with code:', code);
 });
 
+process.on('beforeExit', function(code) {
+
+   // Following code will never execute.
+   /*setTimeout(function() {
+      console.log("This will not run");
+   }, 6000);*/
+  
+   console.log('About to beforeExit with code:', code);
+});
+
 console.log("Program Ended");
